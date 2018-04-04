cb921361c01a1b5ebb4544d83cc8b97dc881ebea
Jonas Sølvsteen <j08lue@gmail.com>
2018-04-04 12:31:12 +0200

    id

--- tests/test_dahiti.py
+++ tests/test_dahiti.py
@@ -5,11 +5,11 @@ def test_list_targets(dahiti_credentials):
     results = dahiti.list_targets(basin='Amazon', **dahiti_credentials)
     assert isinstance(results, list)
     assert len(results) > 0
-    assert 'dahiti_id' in results[0]
+    assert 'id' in results[0]
 
 
 def test_download(dahiti_credentials):
     from dahiti import dahiti
     results = dahiti.list_targets(basin='Amazon', **dahiti_credentials)
-    dahiti_id = results[0]['dahiti_id']
+    dahiti_id = results[0]['id']
     dahiti.download(dahiti_id, **dahiti_credentials)
