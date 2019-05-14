da403f9b893df5ec35920bb9de6998a55de85b5c
Jonas Sølvsteen <j08lue@gmail.com>
2019-05-14 22:38:56 +0200

    remove versioneer

--- setup.py
+++ setup.py
@@ -1,13 +1,11 @@
 from setuptools import setup, find_packages
-import versioneer
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
     name='dahiti',
-    version=versioneer.get_version(),
-    cmdclass=versioneer.get_cmdclass(),
+    version='v1.1.1',
     description='Download water level data from http://dahiti.dgfi.tum.de',
     author='Jonas Solvsteen',
     author_email='josl@dhigroup.com',
