ca2921d4d935a19d046433d2ad5785882955b582
Jonas Sølvsteen <j08lue@gmail.com>
2019-05-14 22:35:23 +0200

    add metadata

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 from setuptools import setup, find_packages
 import versioneer
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='dahiti',
     version=versioneer.get_version(),
@@ -8,6 +11,16 @@ setup(
     description='Download water level data from http://dahiti.dgfi.tum.de',
     author='Jonas Solvsteen',
     author_email='josl@dhigroup.com',
-    url='https://www.dhigroup.com',
+    url='https://github.com/DHI-GRAS/dahiti',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
     packages=find_packages(),
-    install_requires=['requests'])
+    install_requires=[
+        'requests'
+    ]
+)
